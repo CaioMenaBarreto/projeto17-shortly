af0eec18722cb356d68f9373db2fd80ffdb88666
Caio Lucas <clpmb2000@gmail.com>
2023-08-04 21:24:19 -0300

    GET rotas urls/:id e urls/open, feitas

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -16,7 +16,7 @@ export async function postUrlShorten(req, res) {
         const userId = user.rows[0].userId;
 
         const shortUrl = nanoid(8);
-        
+
         await db.query(`INSERT INTO urls (url, shorturl, "userId") VALUES ($1, $2, $3)`, [url, shortUrl, userId]);
 
         const response = await db.query(`SELECT * FROM urls WHERE url = $1`, [url]);
@@ -27,7 +27,51 @@ export async function postUrlShorten(req, res) {
             }
         );
     } catch (error) {
-        console.error('Erro ao criar uma shortUrl:', error.message);
+        console.log('Erro ao criar uma shortUrl:', error.message);
         res.status(500).send({ message: error.message });
     };
 };
+
+export async function getUrlId(req, res) {
+    const { id } = req.params;
+    try {
+
+        const urls = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
+        console.log(urls.rows[0], id);
+
+        if (urls.rows.length === 0) {
+            return res.status(404).send("ShortUrl inexistente.");
+        };
+
+        res.status(200).send({
+            id: urls.rows[0].id,
+            shortUrl: urls.rows[0].shorturl,
+            url: urls.rows[0].url
+        });
+
+    } catch (error) {
+        console.log("Erro ao pegar as Urls:", error.message);
+        res.status(500).send({ message: error.message });
+    };
+};
+
+export async function getUrlOpen(req, res) {
+    const { shortUrl } = req.params;
+    try {
+        const urlQuery = await db.query(`SELECT * FROM urls WHERE shorturl = $1;`, [shortUrl]);
+        const urlData = urlQuery.rows[0];
+
+        if(!urlData){
+            return res.status(404).send("ShortUrl inexistente.");
+        };
+
+        const newVisitCount = urlData.visits + 1;
+        await db.query(`UPDATE urls SET visits = $1 WHERE id = $2;`, [newVisitCount, urlData.id]);
+        
+        res.redirect(urlData.url);
+        
+    } catch(error) {
+        console.log("Erro ao redirecionar para a Url:", error.message);
+        res.status(500).send({ message: error.message });
+    }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import schemaValidation from "../middlewares/schemaValidation.js";
 import urlShorten from "../schemas/urlShorten.schema.js";
-import { postUrlShorten } from "../controllers/url.controllers.js";
+import { getUrlId, getUrlOpen, postUrlShorten } from "../controllers/url.controllers.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", schemaValidation(urlShorten), postUrlShorten);
+urlRouter.get("/urls/:id", getUrlId);
+urlRouter.get("/urls/open/:shortUrl", getUrlOpen);
 
 export default urlRouter;
